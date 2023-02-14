e7e786090fe69b3d3e769820e24e611dd951c363
Zhenya <wowkylgame@gmail.com>
2023-02-14 19:12:53 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию 
+// элементы каждой строки двумерного массива.
+
+
+int m = new Random().Next(3, 5);
+int n = new Random().Next(3, 5);
+
+int[,] array = Generate2DArray(m, n);
+Print2DArray(array);
+System.Console.WriteLine();
+SortArray(array);
+Print2DArray(array);
+
+int[,] Generate2DArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+
+    for(int i = 0; i < result.GetLength(0); i++)
+    {
+        for(int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = new Random().Next(1, 100);
+        }
+    }
+    return result;
+}
+
+void Print2DArray(int[,] arr)
+{
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int min = array[i, k];
+                    array[i, k] = array[i, k + 1];
+                    array[i, k + 1] = min;
+                }
+        }
+    }
+}
